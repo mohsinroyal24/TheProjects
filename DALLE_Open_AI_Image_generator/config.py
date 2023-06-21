5e81824f5e091c581a1f85a50dd092a7138d5969
mohsin <mohsinroyal24@gmail.com>
2023-06-22 00:47:52 +0530

    added all codes

---
+++ DALLE_Open_AI_Image_generator/config.py
@@ -0,0 +1,1 @@
+key = "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"
---
+++ DALLE_Open_AI_Image_generator/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask import render_template
+import openai
+from config import key
+
+openai.api_key = key
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/generateimages/<prompt>')
+def generate(prompt):
+  print("prompt:", prompt)
+  response = openai.Image.create(prompt=prompt, n=3, size="256x256")
+  print(response)
+  return jsonify(response)
+
+
+app.run(host='0.0.0.0', port=81)
