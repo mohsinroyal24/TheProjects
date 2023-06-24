2e5eb4f0cba102946f21440705944e574129f3c6
mohsin <mohsinroyal24@gmail.com>
2023-06-24 21:00:47 +0530

    Added UI

--- ChatGPT Clone/tailwind.config.js
+++ ChatGPT Clone/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./templates/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        chatblack: {50: '#343541'}
+      }
+    },
   },
   plugins: [],
 }
