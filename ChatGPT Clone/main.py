9f9803821edc934943a470f836f44b5d5d8e0c67
mohsin <mohsinroyal24@gmail.com>
2023-06-28 20:29:11 +0530

    added JS

--- ChatGPT Clone/main.py
+++ ChatGPT Clone/main.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
 
+@app.route("/api", methods=["GET","POST"])
+def qa():
+    if request.method=="POST":
+        print(request.form, request.json)
+        question = request.json.get("question")
+        data = {"result": f"Answer of {question}"}
+        return jsonify(data)
+    data = {"result": "Thankyou for asking"}
+    return jsonify(data)
+
 app.run(debug=True)
+
