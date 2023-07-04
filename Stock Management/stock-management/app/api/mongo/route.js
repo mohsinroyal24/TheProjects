e876ad7fe7c1a2fc7d6eb15192059b70230b4651
mohsin <mohsinroyal24@gmail.com>
2023-07-04 22:12:53 +0530

    resolved error

---
+++ Stock Management/stock-management/app/api/mongo/route.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request){
+   
+// Replace the uri string with your connection string.
+const uri = "mongodb+srv://mohsin1:oSh7hv3SC8mkDk5U@cluster0.mo9gerh.mongodb.net/";
+const client = new MongoClient(uri);
+  try {
+    const database = client.db('mohsin');
+    const movies = database.collection('inventory');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+    return NextResponse.json({"a":34,movie})
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+ 
