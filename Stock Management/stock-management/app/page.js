e72fd4beb01c05cd6c92bfc22e58d41e16ea81f2
mohsin <mohsinroyal24@gmail.com>
2023-07-10 04:14:31 +0530

    added the plus minus dynamically changing

--- Stock Management/stock-management/app/page.js
+++ Stock Management/stock-management/app/page.js
@@ -6,22 +6,20 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
-
   const [products, setProducts] = useState([]);
-  const [alert, setAlert] = useState("")
-  const [query, setquery] = useState("")
-  const [loading, setloading] = useState(false)
-  const [loadingaction, setLoadingaction] = useState(false)
-  const [dropdown, setdropdown] = useState([])
+  const [alert, setAlert] = useState("");
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loadingAction, setLoadingAction] = useState(false);
+  const [dropdown, setDropdown] = useState([]);
   const [deletedProduct, setDeletedProduct] = useState(null);
-
+  
 
   // Sample data for demonstration purposes
   const stockData = [
     { id: 1, name: 'Product A', quantity: 10, price: '$10' },
     { id: 2, name: 'Product B', quantity: 5, price: '$15' },
     { id: 3, name: 'Product C', quantity: 8, price: '$20' },
-
   ];
 
   useEffect(() => {
@@ -34,18 +32,21 @@ export default function Home() {
 
     // Check if a product has been deleted
     if (deletedProduct) {
-      const updatedProducts = products.filter(product => product.slug !== deletedProduct);
+      const updatedProducts = products.filter(
+        (product) => product.slug !== deletedProduct
+      );
       setProducts(updatedProducts);
       setDeletedProduct(null);
     }
   }, [deletedProduct]);
 
-
   const buttonAction = async (action, slug, initialQuantity) => {
     if (action === 'delete') {
-      const confirmation = window.confirm('Are you sure you want to delete this product?');
+      const confirmation = window.confirm(
+        'Are you sure you want to delete this product?'
+      );
       if (confirmation) {
-        setLoadingaction(true);
+        setLoadingAction(true);
         const response = await fetch('/api/product', {
           method: 'DELETE',
           headers: {
@@ -55,7 +56,7 @@ export default function Home() {
         });
         const data = await response.json();
         console.log(data);
-        setLoadingaction(false);
+        setLoadingAction(false);
         // Handle the response and update the products state
         if (data.success) {
           // Product deleted successfully
@@ -68,32 +69,29 @@ export default function Home() {
           toast.error('Failed to delete product');
         }
       }
-    }
-    else {
+    } else {
       // Code for plus and minus actions
-      let index = products.findIndex((item) => item.slug == slug)
-      console.log(index)
-      let newProducts = JSON.parse(JSON.stringify(products))
-      if (action == "plus") {
-        newProducts[index].quantity = parseInt(initialQuantity) + 1
+      let index = products.findIndex((item) => item.slug == slug);
+      console.log(index);
+      let newProducts = JSON.parse(JSON.stringify(products));
+      if (action == 'plus') {
+        newProducts[index].quantity = parseInt(initialQuantity) + 1;
+      } else {
+        newProducts[index].quantity = parseInt(initialQuantity) - 1;
       }
-      else {
-        newProducts[index].quantity = parseInt(initialQuantity) - 1
-      }
-      setProducts(newProducts)
+      setProducts(newProducts);
 
-      let indexdrop = dropdown.findIndex((item) => item.slug == slug)
-      console.log(indexdrop, "parse")
-      let newDropdown = JSON.parse(JSON.stringify(dropdown))
-      if (action == "plus") {
-        newDropdown[indexdrop].quantity = parseInt(initialQuantity) + 1
+      let indexdrop = dropdown.findIndex((item) => item.slug == slug);
+      console.log(indexdrop, 'parse');
+      let newDropdown = JSON.parse(JSON.stringify(dropdown));
+      if (action == 'plus') {
+        newDropdown[indexdrop].quantity = parseInt(initialQuantity) + 1;
+      } else {
+        newDropdown[indexdrop].quantity = parseInt(initialQuantity) - 1;
       }
-      else {
-        newDropdown[indexdrop].quantity = parseInt(initialQuantity) - 1
-      }
-      setdropdown(newDropdown)
-      console.log(action, slug)
-      setLoadingaction(true)
+      setDropdown(newDropdown);
+      console.log(action, slug);
+      setLoadingAction(true);
       const response = await fetch('/api/action', {
         method: 'POST',
         headers: {
@@ -101,15 +99,13 @@ export default function Home() {
         },
         body: JSON.stringify({ action, slug, initialQuantity }),
       });
-      let r = await response.json()
-      console.log(r)
-      setLoadingaction(false)
-
+      let r = await response.json();
+      console.log(r);
+      setLoadingAction(false);
     }
   };
 
-
-  const [productForm, setProductForm] = useState({})
+  const [productForm, setProductForm] = useState({});
 
   const addProduct = async (e) => {
     try {
@@ -120,7 +116,7 @@ export default function Home() {
         },
         body: JSON.stringify(productForm),
       });
-  
+
       if (response.ok) {
         // Product added successfully
         console.log('Product added successfully!');
@@ -135,99 +131,160 @@ export default function Home() {
       console.error('Error:', error);
       toast.error('An error occurred');
     }
-  
+
     // fetch all the products again to sync back
     const response = await fetch('/api/product');
     let rjson = await response.json();
     setProducts(rjson.products);
     e.preventDefault();
   };
-  
-
 
   const handleChange = (e) => {
-    setProductForm({ ...productForm, [e.target.name]: e.target.value })
-  }
-
+    setProductForm({ ...productForm, [e.target.name]: e.target.value });
+  };
   const onDropdownEdit = async (e) => {
-    let value = e.target.value
-    setquery(value)
+    let value = e.target.value;
+    setQuery(value);
     if (value.length > 3) {
-      setloading(true)
-      setdropdown([])
-      const response = await fetch('/api/search?query=' + query)
-      let rjson = await response.json()
-      setdropdown(rjson.products)
-      setloading(false)
+      setLoading(true);
+      setDropdown([]);
+      const response = await fetch('/api/search?query=' + value);
+      let rjson = await response.json();
+      setDropdown(rjson.products);
+      setLoading(false);
+    } else {
+      setDropdown([]);
     }
-    else {
-      setdropdown([])
-    }
-  }
-
-
-
+  };
+  
 
   return (
     <>
-      <Header />
+      <Header products={products}
+        setProducts={setProducts}
+        setLoadingAction={setLoadingAction} />
+
 
       {/* Search a product section */}
       <div className="container mx-10 mt-2 p-2">
         <div className="text-green-800 text-center">{alert}</div>
-        <h1 className="font-semibold text-2xl mx-2">Search a product</h1>
+        <h2 className="font-semibold text-2xl mx-2">Search a product</h2>
         <div className="search-form py-3 px-2 flex flex-wrap items-center">
-          <div className="w-full md:w-10/12"> {/* 10/12 width */}
-            {/* <label className="mr-4 font-bold block">Product Name:</label> */}
-            <input onChange={onDropdownEdit} type="text" placeholder="Enter the item to search" className="w-full mt-2 px-4 py-2 rounderd border " />
+          <div className="w-full md:w-10/12">
+            <input
+              onChange={onDropdownEdit}
+              type="text"
+              placeholder="Enter the item to search"
+              className="w-full mt-2 px-4 py-2 rounderd border"
+            />
           </div>
-          <div className="py-2 px-2 w-full md:w-2/12"> {/* 1/12 width */}
-            {/* <label className="mr-4 font-bold block">Category:</label> */}
+          <div className="py-2 px-2 w-full md:w-2/12">
             <select className="w-full mt-2 px-4 py-2 rounderd border">
               <option value="">All</option>
               <option value="category1">Category 1</option>
               <option value="category2">Category 2</option>
-              {/* Add more categories as  needed */}
             </select>
           </div>
         </div>
-        {loading && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
-          <circle cx="25" cy="25" r="20" fill="none" stroke="#000000" strokeWidth="4" strokeDasharray="31.42" strokeDashoffset="0">
-            <animate attributeName="strokeDashoffset" dur="1.5s" repeatCount="indefinite" from="0" to="62.84" />
-          </circle>
-        </svg>
-        }
+        {loading && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 50 50"
+            width="50px"
+            height="50px"
+          >
+            <circle
+              cx="25"
+              cy="25"
+              r="20"
+              fill="none"
+              stroke="#000000"
+              strokeWidth="4"
+              strokeDasharray="31.42"
+              strokeDashoffset="0"
+            >
+              <animate
+                attributeName="strokeDashoffset"
+                dur="1.5s"
+                repeatCount="indefinite"
+                from="0"
+                to="62.84"
+              />
+            </circle>
+          </svg>
+        )}
 
         <div className="dropContainer mx-2 absolute w-[78.5vw] border-1 rounded-md bg-purple-100 ">
-
-          {dropdown.map(item => {
-            return <div key={item.slug}
-              className="contatiner flex justify-between mx-3 p-1 my-1 border-b-2 border-purple-200">
-              <span className="slug">{item.slug} ({item.quantity} available for ₹({item.price})</span>
+          {dropdown.map((item) => (
+            <div
+              key={item.slug}
+              className="contatiner flex justify-between mx-3 p-1 my-1 border-b-2 border-purple-200"
+            >
+              <span className="slug">
+                {item.slug} ({item.quantity} available for ₹{item.price})
+              </span>
               <div className="mx-5">
-                <button onClick={() => { buttonAction("minus", item.slug, item.quantity) }} disabled={loadingaction} className="subtract cursor-pointer inline-block px-3 py-1 mr-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">-</button>
+                <button
+                  onClick={() => {
+                    buttonAction('minus', item.slug, item.quantity);
+                  }}
+                  disabled={loadingAction}
+                  className="subtract cursor-pointer inline-block px-3 py-1 mr-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200"
+                >
+                  -
+                </button>
                 <span className="quantity">{item.quantity}</span>
-                <button onClick={() => { buttonAction("plus", item.slug, item.quantity) }} disabled={loadingaction} className="add  cursor-pointer inline-block px-3 py-1 ml-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">+</button>
+                <button
+                  onClick={() => {
+                    buttonAction('plus', item.slug, item.quantity);
+                  }}
+                  disabled={loadingAction}
+                  className="add  cursor-pointer inline-block px-3 py-1 ml-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200"
+                >
+                  +
+                </button>
               </div>
             </div>
-          })}
+          ))}
         </div>
       </div>
+      
 
       {/* Add a product section */}
       <div className="container mx-10 mt-2 p-2">
         <h1 className="mx-2 font-semibold text-2xl">Add a product</h1>
         <div className="product-form py-3 px-2 mb-6 flex flex-wrap items-center">
-          <div className="py-2 w-full md:w-8/12"> {/* 8/12 width */}
-            <input value={productForm?.slug || ""} name="slug" onChange={handleChange} type="text" placeholder="Enter slug name for the product" className="w-full mt-2 px-4 py-2 rounded border" />
+          <div className="py-2 w-full md:w-8/12">
+            <input
+              value={productForm?.slug || ''}
+              name="slug"
+              onChange={handleChange}
+              type="text"
+              placeholder="Enter slug name for the product"
+              className="w-full mt-2 px-4 py-2 rounded border"
+            />
           </div>
-          <div className="py-2 px-2 w-full md:w-1/12"> {/* 1/12 width */}
-            <input value={productForm?.quantity || ""} name="quantity" onChange={handleChange} type="number" placeholder="Qty" className="w-full mt-2 px-4 py-2 rounded border" />
+          <div className="py-2 px-2 w-full md:w-1/12">
+            <input
+              value={productForm?.quantity || ''}
+              name="quantity"
+              onChange={handleChange}
+              type="number"
+              placeholder="Qty"
+              className="w-full mt-2 px-4 py-2 rounded border"
+            />
           </div>
-          <div className="py-2 px-2 w-full md:w-1/12"> {/* 1/12 width */}
-            <input value={productForm?.price || ""} name="price" onChange={handleChange} type="text" placeholder="Price" className="w-full mt-2 px-4 py-2 rounded border" />
+          <div className="py-2 px-2 w-full md:w-1/12">
+            <input
+              value={productForm?.price || ''}
+              name="price"
+              onChange={handleChange}
+              type="text"
+              placeholder="Price"
+              className="w-full mt-2 px-4 py-2 rounded border"
+            />
           </div>
-          <div className="py-2 px-2 w-full md:w-2/12"> {/* 2/12 width */}
+          <div className="py-2 px-2 w-full md:w-2/12">
             <button
               onClick={addProduct}
               className="bg-purple-500 hover:bg-purple-600 text-white rounded mt-2 px-2 inline-block transition-colors duration-300 ease-in-out"
@@ -239,10 +296,9 @@ export default function Home() {
         </div>
       </div>
 
-
       {/* Current Stock section */}
-      <div className="container mx-10 mt-2 p-2">
-        <h1 className="font-semibold text-2xl mb-2">Current Stock</h1>
+      <div id="current-stock"className="container mx-10 mt-2 p-2">
+        <h2 className="font-semibold text-2xl mb-2">Current Stock</h2>
         <table className="table mt-8 w-full">
           <thead>
             <tr>
@@ -253,26 +309,55 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {products.map(product => (
-              <tr key={product.id} className="text-center">
-                <td className="py-2 px-4 border">{product.slug}</td>
-                <td className="py-2 px-4 border">{product.quantity}</td>
-                <td className="py-2 px-4 border">₹{product.price}</td>
-                <td className="py-2 px-4 border">
-                  <button
-                    onClick={() => buttonAction("delete", product.slug)}
-                    disabled={loadingaction}
-                    className="delete cursor-pointer inline-block px-3 py-1 bg-red-500 text-white font-semibold rounded-lg shadow-md disabled:bg-red-200"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
+            {products
+              .filter((product) => product.quantity > 0) // Filter out products with quantity 0
+              .map((product) => (
+                <tr key={product.id} className="text-center">
+                  <td className="py-2 px-4 border">{product.slug}</td>
+                  <td className="py-2 px-4 border">{product.quantity}</td>
+                  <td className="py-2 px-4 border">₹{product.price}</td>
+                  <td className="py-2 px-4 border">
+                    <button
+                      onClick={() => buttonAction('delete', product.slug)}
+                      disabled={loadingAction}
+                      className="delete cursor-pointer inline-block px-3 py-1 bg-red-500 text-white font-semibold rounded-lg shadow-md disabled:bg-red-200"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
           </tbody>
-
         </table>
       </div>
+      <br></br>
+      <br></br>
+      {/* Out of Stock section */}
+      <div  id="out-of-stock" className="container mx-10 mt-2 p-2">
+        <h2 className="font-semibold text-2xl mb-2">Out of Stock Items</h2>
+        <table className="table mt-8 w-full">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border">Name</th>
+              <th className="py-2 px-4 border">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => {
+              if (product.quantity === 0) {
+                return (
+                  <tr key={product.id} className="text-center">
+                    <td className="py-2 px-4 border">{product.slug}</td>
+                    <td className="py-2 px-4 border">₹{product.price}</td>
+                  </tr>
+                );
+              }
+              return null;
+            })}
+          </tbody>
+        </table>
+      </div>
+
       <ToastContainer />
     </>
   );
--- Stock Management/stock-management/components/Header.js
+++ Stock Management/stock-management/components/Header.js
@@ -1,25 +1,160 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Link from 'next/link';
 
-const Header = () => {
+const Header = ({ products, setProducts, setLoadingAction }) => {
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [dropdown, setDropdown] = useState([]);
+
+  const scrollToSection = (id) => {
+    if (id === 'top') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  };
+
+  const handleSearch = async (e) => {
+    const value = e.target.value;
+    setQuery(value);
+    if (value.length > 3) {
+      setLoading(true);
+      setDropdown([]);
+      const response = await fetch('/api/search?query=' + value);
+      const data = await response.json();
+      setDropdown(data.products);
+      setLoading(false);
+    } else {
+      setDropdown([]);
+    }
+  };
+
+  const buttonAction = async (action, slug, initialQuantity) => {
+    let index = products.findIndex((item) => item.slug === slug);
+    let newProducts = [...products];
+    if (action === 'plus') {
+      newProducts[index].quantity = parseInt(initialQuantity) + 1;
+    } else if (action === 'minus') {
+      newProducts[index].quantity = parseInt(initialQuantity) - 1;
+    }
+    setProducts(newProducts);
+
+    let indexdrop = dropdown.findIndex((item) => item.slug === slug);
+    let newDropdown = [...dropdown];
+    if (action === 'plus') {
+      newDropdown[indexdrop].quantity = parseInt(initialQuantity) + 1;
+    } else if (action === 'minus') {
+      newDropdown[indexdrop].quantity = parseInt(initialQuantity) - 1;
+    }
+    setDropdown(newDropdown);
+
+    setLoadingAction(true);
+    const response = await fetch('/api/action', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ action, slug, initialQuantity }),
+    });
+    const data = await response.json();
+    setLoadingAction(false);
+    console.log(data);
+  };
+
   return (
-    <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span className="ml-3 text-xl">Stock Management System</span>
-    </a>
-    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">First Link</a>
-      <a className="mr-5 hover:text-gray-900">Second Link</a>
-      <a className="mr-5 hover:text-gray-900">Third Link</a>
-      <a className="mr-5 hover:text-gray-900">Fourth Link</a>
-    </nav>
+    <header className="text-gray-600 body-font sticky top-0 z-10 bg-white shadow">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <Link href="/" passHref className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">Stock Management System</span>
+        </Link>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center w-full">
+          <Link href="/" passHref className="mr-5 hover:text-gray-900" onClick={() => scrollToSection('top')}>
+            Home
+          </Link>
+          <Link href="#current-stock" passHref className="mr-5 hover:text-gray-900">
+            Current stock
+          </Link>
+          <Link href="#out-of-stock" passHref className="mr-5 hover:text-gray-900">
+            Out of stock
+          </Link>
+          <div className="relative flex-grow w-1/5">
+            <input
+              type="text"
+              value={query}
+              onChange={handleSearch}
+              placeholder="Enter the item to search"
+              className="w-full mt-2 px-4 py-2 border rounded"
+            />
+            {loading && (
+              <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                <svg
+                  className="animate-spin h-5 w-5 text-gray-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              </div>
+            )}
+            {dropdown.length > 0 && (
+              <div className="absolute left-0 right-0 mt-1 bg-white border rounded-b shadow-md">
+                {dropdown.map((item) => (
+                  <div
+                    key={item.slug}
+                    className="p-2 flex items-center justify-between hover:bg-gray-100 cursor-pointer"
+                    onClick={() => scrollToSection(item.slug)}
+                  >
+                    <div>{item.slug}</div>
+                    <div>
+                      <button
+                        onClick={() => buttonAction('minus', item.slug, item.quantity)}
+                        className="subtract cursor-pointer inline-block px-3 py-1 mr-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md"
+                      >
+                        -
+                      </button>
+                      <span>{item.quantity}</span>
+                      <button
+                        onClick={() => buttonAction('plus', item.slug, item.quantity)}
+                        className="add cursor-pointer inline-block px-3 py-1 ml-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md"
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          <select className="w-1/5 md:w-24 mt-2 px-4 py-2 rounded border">
+            <option value="">All</option>
+            <option value="category1">Category 1</option>
+            <option value="category2">Category 2</option>
+          </select>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-  </div>
-</header>
-  )
-}
-
-export default Header
+export default Header;
