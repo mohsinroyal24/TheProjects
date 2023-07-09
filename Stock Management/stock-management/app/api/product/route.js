4c9a2a9398b8f886fba1145ab9807e8d303783fd
mohsin <mohsinroyal24@gmail.com>
2023-07-09 23:11:24 +0530

    completed project

--- Stock Management/stock-management/app/api/product/route.js
+++ Stock Management/stock-management/app/api/product/route.js
@@ -2,39 +2,66 @@ import { MongoClient } from "mongodb";
 import { NextResponse } from "next/server";
 
 
-export async function GET(request){
-   
-// Replace the uri string with your connection string.
-const uri = "mongodb+srv://mohsin1:oSh7hv3SC8mkDk5U@cluster0.mo9gerh.mongodb.net/";
-const client = new MongoClient(uri);
+export async function GET(request) {
+
+  // Replace the uri string with your connection string.
+  const uri = "mongodb+srv://mohsin1:oSh7hv3SC8mkDk5U@cluster0.mo9gerh.mongodb.net/";
+  const client = new MongoClient(uri);
   try {
     const database = client.db('stock');
     const inventory = database.collection('inventory');
     const query = {}
     const products = await inventory.find(query).toArray();
-    return NextResponse.json({sucess: true, products})
+    return NextResponse.json({ sucess: true, products })
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
- 
-export async function POST(request){
 
-    let body = await request.json()
-    console.log(body) 
-   
-    // Replace the uri string with your connection string.
-    const uri = "mongodb+srv://mohsin1:oSh7hv3SC8mkDk5U@cluster0.mo9gerh.mongodb.net/";
-    const client = new MongoClient(uri);
-      try {
-        const database = client.db('stock');
-        const inventory = database.collection('inventory');
-        const product = await inventory.insertOne(body);
-        return NextResponse.json({product, ok:true})
-      } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-      }
-      }
-    
+export async function POST(request) {
+
+  let body = await request.json()
+  console.log(body)
+
+  // Replace the uri string with your connection string.
+  const uri = "mongodb+srv://mohsin1:oSh7hv3SC8mkDk5U@cluster0.mo9gerh.mongodb.net/";
+  const client = new MongoClient(uri);
+  try {
+    const database = client.db('stock');
+    const inventory = database.collection('inventory');
+    const product = await inventory.insertOne(body);
+    return NextResponse.json({ product, ok: true })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+export async function DELETE(request) {
+  let body = await request.json();
+  console.log(body);
+
+  // Replace the uri string with your connection string.
+  const uri = "mongodb+srv://mohsin1:oSh7hv3SC8mkDk5U@cluster0.mo9gerh.mongodb.net/";
+  const client = new MongoClient(uri);
+
+  try {
+    const database = client.db('stock');
+    const inventory = database.collection('inventory');
+    const deleteResult = await inventory.deleteOne({ slug: body.slug });
+
+    if (deleteResult.deletedCount === 1) {
+      // return NextResponse.json({ success: true, message: 'Product deleted successfully' });
+      return NextResponse.json({ success: true, slug: body.slug });
+
+    } else {
+      return NextResponse.json({ success: false, message: 'Product not found' });
+    }
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+
--- Stock Management/stock-management/app/page.js
+++ Stock Management/stock-management/app/page.js
@@ -2,6 +2,8 @@
 import Image from 'next/image';
 import Header from '@/components/Header';
 import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
 
@@ -9,26 +11,10 @@ export default function Home() {
   const [alert, setAlert] = useState("")
   const [query, setquery] = useState("")
   const [loading, setloading] = useState(false)
-  const [dropdown, setdropdown] = useState([
-    {
-      "_id": "64a6d2e9717b9b1257bf52cf",
-      "slug": "Baa",
-      "quantity": "2",
-      "price": "200"
-    },
-    {
-      "_id": "64a6d2e9717b9b1257bf52cf",
-      "slug": "Baa2",
-      "quantity": "2",
-      "price": "200"
-    },
-    {
-      "_id": "64a6d2e9717b9b1257bf52cf",
-      "slug": "Baa22",
-      "quantity": "2",
-      "price": "200"
-    },
-])
+  const [loadingaction, setLoadingaction] = useState(false)
+  const [dropdown, setdropdown] = useState([])
+  const [deletedProduct, setDeletedProduct] = useState(null);
+
 
   // Sample data for demonstration purposes
   const stockData = [
@@ -40,20 +26,92 @@ export default function Home() {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('/api/product')
-      let rjson = await response.json()
-      setProducts(rjson.products)
-    }
-    fetchProducts()
-  }, [])
+      const response = await fetch('/api/product');
+      let rjson = await response.json();
+      setProducts(rjson.products);
+    };
+    fetchProducts();
 
+    // Check if a product has been deleted
+    if (deletedProduct) {
+      const updatedProducts = products.filter(product => product.slug !== deletedProduct);
+      setProducts(updatedProducts);
+      setDeletedProduct(null);
+    }
+  }, [deletedProduct]);
+
+
+  const buttonAction = async (action, slug, initialQuantity) => {
+    if (action === 'delete') {
+      const confirmation = window.confirm('Are you sure you want to delete this product?');
+      if (confirmation) {
+        setLoadingaction(true);
+        const response = await fetch('/api/product', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ slug }),
+        });
+        const data = await response.json();
+        console.log(data);
+        setLoadingaction(false);
+        // Handle the response and update the products state
+        if (data.success) {
+          // Product deleted successfully
+          console.log('Product deleted successfully');
+          setDeletedProduct(data.slug);
+          toast.success('Product deleted successfully');
+        } else {
+          // Product not found or other error occurred
+          console.error(data.message);
+          toast.error('Failed to delete product');
+        }
+      }
+    }
+    else {
+      // Code for plus and minus actions
+      let index = products.findIndex((item) => item.slug == slug)
+      console.log(index)
+      let newProducts = JSON.parse(JSON.stringify(products))
+      if (action == "plus") {
+        newProducts[index].quantity = parseInt(initialQuantity) + 1
+      }
+      else {
+        newProducts[index].quantity = parseInt(initialQuantity) - 1
+      }
+      setProducts(newProducts)
+
+      let indexdrop = dropdown.findIndex((item) => item.slug == slug)
+      console.log(indexdrop, "parse")
+      let newDropdown = JSON.parse(JSON.stringify(dropdown))
+      if (action == "plus") {
+        newDropdown[indexdrop].quantity = parseInt(initialQuantity) + 1
+      }
+      else {
+        newDropdown[indexdrop].quantity = parseInt(initialQuantity) - 1
+      }
+      setdropdown(newDropdown)
+      console.log(action, slug)
+      setLoadingaction(true)
+      const response = await fetch('/api/action', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ action, slug, initialQuantity }),
+      });
+      let r = await response.json()
+      console.log(r)
+      setLoadingaction(false)
+
+    }
+  };
 
 
   const [productForm, setProductForm] = useState({})
 
   const addProduct = async (e) => {
-    e.preventDefault();
-
     try {
       const response = await fetch('/api/product', {
         method: 'POST',
@@ -62,37 +120,52 @@ export default function Home() {
         },
         body: JSON.stringify(productForm),
       });
-
+  
       if (response.ok) {
-        // Product added successfully, you can update the stockData state if needed
+        // Product added successfully
         console.log('Product added successfully!');
-        setAlert("Your product has been added")
-        setProductForm({})
-        // Clear the product form fields after successful addition
+        setProductForm({});
+        toast.success('Product added successfully');
       } else {
         // Handle error if the server responds with an error status
         console.error('Error adding product:');
+        toast.error('Failed to add product');
       }
     } catch (error) {
       console.error('Error:', error);
+      toast.error('An error occurred');
     }
+  
+    // fetch all the products again to sync back
+    const response = await fetch('/api/product');
+    let rjson = await response.json();
+    setProducts(rjson.products);
+    e.preventDefault();
   };
+  
+
 
   const handleChange = (e) => {
     setProductForm({ ...productForm, [e.target.name]: e.target.value })
   }
 
-  const onDropdownEdit =async (e) =>{
-    setquery(e.target.value)
-    if(!loading){
+  const onDropdownEdit = async (e) => {
+    let value = e.target.value
+    setquery(value)
+    if (value.length > 3) {
       setloading(true)
       setdropdown([])
-    const response = await fetch('/api/search?query='+query)
-    let rjson = await response.json()
-    setdropdown(rjson.products)
-    setloading(false)
+      const response = await fetch('/api/search?query=' + query)
+      let rjson = await response.json()
+      setdropdown(rjson.products)
+      setloading(false)
+    }
+    else {
+      setdropdown([])
+    }
   }
-}
+
+
 
 
   return (
@@ -118,46 +191,54 @@ export default function Home() {
             </select>
           </div>
         </div>
-      {loading && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
-  <circle cx="25" cy="25" r="20" fill="none" stroke="#000000" stroke-width="4" stroke-dasharray="31.42" stroke-dashoffset="0">
-    <animate attributeName="stroke-dashoffset" dur="1.5s" repeatCount="indefinite" from="0" to="62.84" />
-  </circle>
-</svg>
-}
+        {loading && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
+          <circle cx="25" cy="25" r="20" fill="none" stroke="#000000" strokeWidth="4" strokeDasharray="31.42" strokeDashoffset="0">
+            <animate attributeName="strokeDashoffset" dur="1.5s" repeatCount="indefinite" from="0" to="62.84" />
+          </circle>
+        </svg>
+        }
 
-        {dropdown.map(item => {
-          return <div key={item.slug}
-          className="contatiner flex justify-between mx-3 bg-purple-200 my-3">
-            <span className="slug">{item.slug}</span>
-            <span className="price">{item.price}</span>
-            <span className="quantity">{item.quantity}</span>
-          </div>
-        })}
+        <div className="dropContainer mx-2 absolute w-[78.5vw] border-1 rounded-md bg-purple-100 ">
+
+          {dropdown.map(item => {
+            return <div key={item.slug}
+              className="contatiner flex justify-between mx-3 p-1 my-1 border-b-2 border-purple-200">
+              <span className="slug">{item.slug} ({item.quantity} available for ₹({item.price})</span>
+              <div className="mx-5">
+                <button onClick={() => { buttonAction("minus", item.slug, item.quantity) }} disabled={loadingaction} className="subtract cursor-pointer inline-block px-3 py-1 mr-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">-</button>
+                <span className="quantity">{item.quantity}</span>
+                <button onClick={() => { buttonAction("plus", item.slug, item.quantity) }} disabled={loadingaction} className="add  cursor-pointer inline-block px-3 py-1 ml-2 bg-purple-500 text-white font-semibold rounded-lg shadow-md disabled:bg-purple-200">+</button>
+              </div>
+            </div>
+          })}
+        </div>
       </div>
 
       {/* Add a product section */}
       <div className="container mx-10 mt-2 p-2">
         <h1 className="mx-2 font-semibold text-2xl">Add a product</h1>
         <div className="product-form py-3 px-2 mb-6 flex flex-wrap items-center">
-          <div className="py-2  w-full md:w-9/12"> {/* 10/12 width */}
-            {/* <label className="mr-4 font-bold block">Product Slug:</label> */}
-            <input value={productForm?.slug || ""} name='slug' onChange={handleChange} type="text" placeholder="Enter slug name for the product" className="w-full mt-2 px-4 py-2 rounded border" />
+          <div className="py-2 w-full md:w-8/12"> {/* 8/12 width */}
+            <input value={productForm?.slug || ""} name="slug" onChange={handleChange} type="text" placeholder="Enter slug name for the product" className="w-full mt-2 px-4 py-2 rounded border" />
           </div>
           <div className="py-2 px-2 w-full md:w-1/12"> {/* 1/12 width */}
-            {/* <label className="mr-4 font-bold block">Quantity:</label> */}
-            <input value={productForm?.quantity || ""} name='quantity' onChange={handleChange} type="number" placeholder="Qty" className="w-full mt-2 px-4 py-2 rounded border" />
+            <input value={productForm?.quantity || ""} name="quantity" onChange={handleChange} type="number" placeholder="Qty" className="w-full mt-2 px-4 py-2 rounded border" />
           </div>
           <div className="py-2 px-2 w-full md:w-1/12"> {/* 1/12 width */}
-            {/* <label className="mr-4 font-bold block">Price:</label> */}
-            <input value={productForm?.price || ""} name='price' onChange={handleChange} type="text" placeholder="Price" className="w-full mt-2 px-4 py-2 rounded border" />
+            <input value={productForm?.price || ""} name="price" onChange={handleChange} type="text" placeholder="Price" className="w-full mt-2 px-4 py-2 rounded border" />
           </div>
-
-          <button onClick={addProduct} className="bg-green-600 text-white rounded mt-3 md:mt-2 py-2 px-2 w-full md:w-1/12">
-            Add Product
-          </button>
-
+          <div className="py-2 px-2 w-full md:w-2/12"> {/* 2/12 width */}
+            <button
+              onClick={addProduct}
+              className="bg-purple-500 hover:bg-purple-600 text-white rounded mt-2 px-2 inline-block transition-colors duration-300 ease-in-out"
+              style={{ height: '2.5rem' }}
+            >
+              Add Product
+            </button>
+          </div>
         </div>
       </div>
+
 
       {/* Current Stock section */}
       <div className="container mx-10 mt-2 p-2">
@@ -168,20 +249,31 @@ export default function Home() {
               <th className="py-2 px-4 border">Name</th>
               <th className="py-2 px-4 border">Quantity</th>
               <th className="py-2 px-4 border">Price</th>
+              <th className="py-2 px-4 border">Remove Product</th>
             </tr>
           </thead>
           <tbody>
-
-            {products.map(product => {
-              return <tr key={product.slug} className="text-center">
+            {products.map(product => (
+              <tr key={product.id} className="text-center">
                 <td className="py-2 px-4 border">{product.slug}</td>
                 <td className="py-2 px-4 border">{product.quantity}</td>
                 <td className="py-2 px-4 border">₹{product.price}</td>
+                <td className="py-2 px-4 border">
+                  <button
+                    onClick={() => buttonAction("delete", product.slug)}
+                    disabled={loadingaction}
+                    className="delete cursor-pointer inline-block px-3 py-1 bg-red-500 text-white font-semibold rounded-lg shadow-md disabled:bg-red-200"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
-            })}
+            ))}
           </tbody>
+
         </table>
       </div>
+      <ToastContainer />
     </>
   );
 }
