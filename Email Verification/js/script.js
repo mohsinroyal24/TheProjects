5e22bccbbbe32155e126e2b0afcf0236b412da5a
mohsin <mohsinroyal24@gmail.com>
2023-09-18 00:34:26 +0530

    completed project

--- Email Verification/js/script.js
+++ Email Verification/js/script.js
@@ -1,28 +1,23 @@
 console.log("This is my script")
 
+submitBtn.addEventListener("click", async(e) => {
+    e.preventDefault()
+    console.log("Clicked")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let email = document.getElementById("username").value
+    let key = "ema_live_V6O1djadGGY9INDArkDFR3whSIoaQaxzODVXFIjb"
 
-let result = {
-    
-        "tag": "",
-        "free": true,
-        "role": false,
-        "user": "mohsinroyal24",
-        "email": "mohsinroyal24@gmail.com",
-        "score": 0.64,
-        "state": "deliverable",
-        "domain": "gmail.com",
-        "reason": "valid_mailbox",
-        "mx_found": true,
-        "catch_all": null,
-        "disposable": false,
-        "smtp_check": true,
-        "did_you_mean": "",
-        "format_valid": true
-}
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key]!=="" && result[key]!= " ")
+        str = str + `<div>${key}: ${result[key]}</div>`
+    }
 
-let str = ``
-for (key of Object.keys(result)){
-    str = str + `<div>${key}: ${result[key]}</div>`
-}
+    resultCont.innerHTML = str
 
-resultCont.innerHTML = str
+})
+
+
---
+++ Email Verification/readme.md
@@ -0,0 +1,35 @@
+# iValidate - Email Validator Project
+
+This project is a simple web application built using HTML, CSS, and JavaScript to validate email addresses. It utilizes the emailvalidation.io API to provide information about the given email address.
+
+## Getting Started
+
+To run this project locally, follow these steps:
+
+1. Clone the repository:
+Open the index.html file in your preferred web browser.
+
+
+## Usage
+1. Enter the email address you want to validate in the input field.
+2. Click the "Submit" button.
+3. The application will display information about the provided email address.
+
+## Project Structure
+1. index.html: Contains the structure of the web page.
+2. css/styles.css: Contains the project's styles.
+3. js/script.js: Contains the JavaScript code for API interaction and handling user input.
+
+## Dependencies
+This project uses the Poppins font from Google Fonts.
+It also relies on the emailvalidation.io API to validate email addresses.
+
+
+## Acknowledgments
+Thanks to emailvalidation.io for providing the API used in this project.
+
+## Author
+Mohsin
+
+
+Feel free to customize this template according to your specific project details. Make sure to replace the placeholders with your actual information and update any URLs or references as needed. Good luck with your project!
